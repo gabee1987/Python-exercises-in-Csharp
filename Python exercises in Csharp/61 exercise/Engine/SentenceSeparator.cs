53463223a1801e72fc491ca47afae26e67fc8059
Gabor Koncz <gabee1987@gmail.com>
2020-11-01 11:48:37 +0100

    Main logic class created, console wrapping created

--- Python exercises in Csharp/61 exercise/Engine/SentenceSeparator.cs
+++ Python exercises in Csharp/61 exercise/Engine/SentenceSeparator.cs
@@ -6,6 +6,13 @@ namespace _61_exercise.Engine
 {
     public class SentenceSeparator
     {
+        public static string SeparateText( string baseText )
+        {
+            string result = string.Empty;
 
+
+
+            return result;
+        }
     }
 }
--- Python exercises in Csharp/61 exercise/Program.cs
+++ Python exercises in Csharp/61 exercise/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _61_exercise.Engine;
+using System;
+using System.IO;
 
 namespace _61_exercise
 {
@@ -6,7 +8,15 @@ namespace _61_exercise
     {
         static void Main( string[] args )
         {
-            Console.WriteLine( "Hello World!" );
+            String originalText = File.ReadAllText( @"..\..\Resources\TestText.txt" );
+            if ( string.IsNullOrEmpty( originalText ) )
+                Console.WriteLine( "File couldnt found!" );
+
+            Console.WriteLine( "Press enter to split the  text!" );
+            Console.WriteLine( string.Format("This is the original text: {0}", originalText ) );
+            Console.ReadKey();
+
+            string result = SentenceSeparator.SeparateText(originalText);
         }
     }
 }
