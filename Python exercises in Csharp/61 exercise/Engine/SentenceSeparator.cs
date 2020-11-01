37a42941e45feaeb04436b1319c26e46c0c77974
Gabor Koncz <gabee1987@gmail.com>
2020-11-01 11:38:42 +0100

    61 exercise project created

---
+++ Python exercises in Csharp/61 exercise/Engine/SentenceSeparator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _61_exercise.Engine
+{
+    public class SentenceSeparator
+    {
+
+    }
+}
---
+++ Python exercises in Csharp/61 exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _61_exercise
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
