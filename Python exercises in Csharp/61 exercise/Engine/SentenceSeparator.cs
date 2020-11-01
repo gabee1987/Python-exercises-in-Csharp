dfbc94e6b2bee418f2aa2aae3fa05bbfd0b24544
Gabor Koncz <gabee1987@gmail.com>
2020-11-01 12:20:08 +0100

    ? and ! handled

--- Python exercises in Csharp/61 exercise/Engine/SentenceSeparator.cs
+++ Python exercises in Csharp/61 exercise/Engine/SentenceSeparator.cs
@@ -9,8 +9,21 @@ namespace _61_exercise.Engine
         public static string SeparateText( string baseText )
         {
             string result = string.Empty;
+            string[] signs = new string[2]{"?", "!" };
+            var words = baseText.Split(' ');
+            //Console.WriteLine( words );
 
+            StringBuilder stb = new StringBuilder();
 
+            for ( int i = 0; i < words.Length; i++ )
+            {
+                stb.Append( words[i] );
+                if ( words[i].Contains( signs[0] ) || words[i].Contains( signs[1] ) )
+                    stb.Append( @"\n" );
+                //else if (  )
+                else
+                    stb.Append( ' ' );
+            }
 
             return result;
         }
--- Python exercises in Csharp/61 exercise/Program.cs
+++ Python exercises in Csharp/61 exercise/Program.cs
@@ -8,15 +8,20 @@ namespace _61_exercise
     {
         static void Main( string[] args )
         {
-            String originalText = File.ReadAllText( @"..\..\Resources\TestText.txt" );
+            //string fileName = "TestText.txt";
+            //string path = Path.Combine(Environment.CurrentDirectory, @"Resource\", fileName);
+
+            String originalText = File.ReadAllText( @"..\..\..\Resources\TestText.txt" );
             if ( string.IsNullOrEmpty( originalText ) )
                 Console.WriteLine( "File couldnt found!" );
 
             Console.WriteLine( "Press enter to split the  text!" );
-            Console.WriteLine( string.Format("This is the original text: {0}", originalText ) );
+            Console.WriteLine( string.Format(@"This is the original text: \n {0}", originalText ) );
             Console.ReadKey();
 
             string result = SentenceSeparator.SeparateText(originalText);
+            Console.ReadKey();
+
         }
     }
 }
