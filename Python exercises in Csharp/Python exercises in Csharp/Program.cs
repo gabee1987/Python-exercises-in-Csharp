7769613b23e45f1303d1b331492d1bc4abe5ab3f
Gabor Koncz <gabee1987@gmail.com>
2020-11-01 11:24:07 +0100

    Project created

---
+++ Python exercises in Csharp/Python exercises in Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Python_exercises_in_Csharp
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
